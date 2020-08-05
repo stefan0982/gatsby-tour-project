7556f3d43dafdadf1056822d9ef96221160322b1
Stefan <stefan.c.0608@mail.ru>
2020-08-05 19:00:57 +0300

    card twitter

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -32,6 +32,11 @@ const SEO = ({ title, description }) => {
         content={ description || siteDescription }
       />
       <meta name="image" content={image}/>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername}/>
+      <meta  name="twitter:title" content={siteTitle} />
+      <meta  name="twitter:description" content={description || siteDescription} />
+      <meta  name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
